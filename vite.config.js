67dd9d8a27a94aac94823703d8abe915843b3f1b
lakshayjeet47 <lakshayjeet13@gmail.com>
2026-09-22 15:49:47 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // e.g. if your repo is github.com/you/falcon-sports, base should be '/falcon-sports/'
 export default defineConfig({
   plugins: [react()],
-  base: '/falcon-sports/',
+  base: '/Falcon_Sports/',
 })
